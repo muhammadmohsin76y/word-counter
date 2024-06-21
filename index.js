cb2743cf1ab841abca3a989b06c2afd007659f10
muhammadmohsin76y <muhammadmohsin76y@gmail.com>
2024-06-22 02:43:42 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a sentence to count the word:"
+    }
+]);
+const words = answers.sentence.trim().split("");
+//print the array of words to the console
+console.log(words);
+// print the word count of the sentance to the console
+console.log(`The sentence word count is ${words.length}`);
